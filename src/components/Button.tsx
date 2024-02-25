41a1740391d5fe060ce1a8f6e88a101f13c715f0
unknown <yahiasbahi1@gmail.com>
2024-02-25 15:49:32 +0300

    Fix build issues

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
-const Button = ({ styles }: { styles: string }) => {
+const Button = () => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none  rounded-[10px]`}
     >
       Get Started
     </button>
